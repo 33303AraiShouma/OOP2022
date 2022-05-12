f5c5a91fbe95d52d0b921c05713fc26c4ca5166f
33303新井翔夢 <33303@ojs.ac.jp>
2022-05-12 14:19:51 +0900

    問題２－２

---
+++ Chapter02/Execise03/Sale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execise03 {
+    class Sale {
+        //商品カテゴリー
+        public string ProductCategory { get; set; }
+        //売上高
+        public int Amount { get; set; }
+    }
+}
