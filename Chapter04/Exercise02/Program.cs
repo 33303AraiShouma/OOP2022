090b4816fdf2682994614d808da7cd0469722aa5
33303新井翔夢 <33303@ojs.ac.jp>
2022-05-24 13:40:00 +0900

    問題４－２作成準備 問題4－1クラスライブラリ化

---
+++ Chapter04/Exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var ym = new YearMonth(2022,5);
+
+            var check = ym.Is21Century;
+            var test = ym.addOneMonth();
+
+            Console.WriteLine(ym);
+        }
+    }
+}
