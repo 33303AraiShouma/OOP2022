ce947ddb3b11075ace482d78d8ad48a15eba7d12
33303新井翔夢 <33303@ojs.ac.jp>
2022-06-07 13:14:47 +0900

    FormAppSample

---
+++ FormAppSample/Sample0603/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Sample0603 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btPush = new System.Windows.Forms.Button();
+            this.tbSuu1 = new System.Windows.Forms.TextBox();
+            this.tbSuu2 = new System.Windows.Forms.TextBox();
+            this.tbAns = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btPush
+            // 
+            this.btPush.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btPush.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btPush.Location = new System.Drawing.Point(173, 83);
+            this.btPush.Name = "btPush";
+            this.btPush.Size = new System.Drawing.Size(267, 168);
+            this.btPush.TabIndex = 0;
+            this.btPush.Text = "計算";
+            this.btPush.UseVisualStyleBackColor = false;
+            this.btPush.Click += new System.EventHandler(this.btPush_Click);
+            // 
+            // tbSuu1
+            // 
+            this.tbSuu1.Location = new System.Drawing.Point(117, 30);
+            this.tbSuu1.Name = "tbSuu1";
+            this.tbSuu1.Size = new System.Drawing.Size(94, 19);
+            this.tbSuu1.TabIndex = 1;
+            // 
+            // tbSuu2
+            // 
+            this.tbSuu2.Location = new System.Drawing.Point(243, 30);
+            this.tbSuu2.Name = "tbSuu2";
+            this.tbSuu2.Size = new System.Drawing.Size(94, 19);
+            this.tbSuu2.TabIndex = 1;
+            // 
+            // tbAns
+            // 
+            this.tbAns.Location = new System.Drawing.Point(360, 30);
+            this.tbAns.Name = "tbAns";
+            this.tbAns.Size = new System.Drawing.Size(96, 19);
+            this.tbAns.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 344);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(445, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "=";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(117, 342);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 19);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(292, 342);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 19);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(485, 342);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 19);
+            this.numericUpDown3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 395);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbAns);
+            this.Controls.Add(this.tbSuu2);
+            this.Controls.Add(this.tbSuu1);
+            this.Controls.Add(this.btPush);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btPush;
+        private System.Windows.Forms.TextBox tbSuu1;
+        private System.Windows.Forms.TextBox tbSuu2;
+        private System.Windows.Forms.TextBox tbAns;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ FormAppSample/Sample0603/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0603 {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btPush_Click(object sender, EventArgs e)
+        {
+           // tbAns.Text= (int.Parse(tbSuu1.Text) + int.Parse(tbSuu2.Text)).ToString();
+
+        }
+    }
+}
