9009af83ad18d489d711721ba2fabf28fb0d301d
33303新井翔夢 <33303@ojs.ac.jp>
2022-06-09 14:16:12 +0900

    adressbook

--- FormAppSample/Sample0607/Form1.Designer.cs
+++ FormAppSample/Sample0607/Form1.Designer.cs
@@ -27,112 +27,88 @@ namespace Sample0607 {
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbNum1 = new System.Windows.Forms.TextBox();
-            this.tbNum2 = new System.Windows.Forms.TextBox();
-            this.tbMod = new System.Windows.Forms.TextBox();
+            this.btRandom = new System.Windows.Forms.Button();
+            this.Number1 = new System.Windows.Forms.NumericUpDown();
+            this.text = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.tbAns = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Number2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.Number1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Number2)).BeginInit();
             this.SuspendLayout();
             // 
-            // tbNum1
+            // btRandom
             // 
-            this.tbNum1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.tbNum1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.tbNum1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.tbNum1.Location = new System.Drawing.Point(28, 57);
-            this.tbNum1.Name = "tbNum1";
-            this.tbNum1.Size = new System.Drawing.Size(100, 31);
-            this.tbNum1.TabIndex = 0;
+            this.btRandom.Location = new System.Drawing.Point(618, 225);
+            this.btRandom.Name = "btRandom";
+            this.btRandom.Size = new System.Drawing.Size(174, 55);
+            this.btRandom.TabIndex = 0;
+            this.btRandom.Text = "乱数取得";
+            this.btRandom.UseVisualStyleBackColor = true;
+            this.btRandom.Click += new System.EventHandler(this.btRandom_Click);
             // 
-            // tbNum2
+            // Number1
             // 
-            this.tbNum2.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.tbNum2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.tbNum2.Location = new System.Drawing.Point(273, 57);
-            this.tbNum2.Name = "tbNum2";
-            this.tbNum2.Size = new System.Drawing.Size(100, 31);
-            this.tbNum2.TabIndex = 0;
+            this.Number1.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Number1.Location = new System.Drawing.Point(315, 50);
+            this.Number1.Name = "Number1";
+            this.Number1.Size = new System.Drawing.Size(209, 71);
+            this.Number1.TabIndex = 1;
+            
             // 
-            // tbMod
+            // text
             // 
-            this.tbMod.BackColor = System.Drawing.SystemColors.Info;
-            this.tbMod.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.tbMod.Location = new System.Drawing.Point(688, 57);
-            this.tbMod.Name = "tbMod";
-            this.tbMod.Size = new System.Drawing.Size(100, 31);
-            this.tbMod.TabIndex = 0;
+            this.text.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.text.Location = new System.Drawing.Point(251, 442);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(519, 44);
+            this.text.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(157, 57);
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(205, 75);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 33);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "÷";
+            this.label1.Size = new System.Drawing.Size(98, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "１から\r\n";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(379, 57);
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(539, 75);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 33);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "=";
+            this.label2.Size = new System.Drawing.Size(387, 37);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "の中で好きな数字を入力\r\n";
             // 
-            // label3
+            // Number2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label3.ForeColor = System.Drawing.Color.Blue;
-            this.label3.Location = new System.Drawing.Point(583, 55);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 33);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "あまり";
-            // 
-            // tbAns
-            // 
-            this.tbAns.BackColor = System.Drawing.SystemColors.Info;
-            this.tbAns.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.tbAns.Location = new System.Drawing.Point(477, 57);
-            this.tbAns.Name = "tbAns";
-            this.tbAns.Size = new System.Drawing.Size(100, 31);
-            this.tbAns.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(536, 191);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 47);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "計算";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button_Click);
+            this.Number2.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Number2.Location = new System.Drawing.Point(330, 225);
+            this.Number2.Name = "Number2";
+            this.Number2.Size = new System.Drawing.Size(229, 71);
+            this.Number2.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1011, 650);
+            this.Controls.Add(this.Number2);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.tbAns);
-            this.Controls.Add(this.tbMod);
-            this.Controls.Add(this.tbNum2);
-            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.Number1);
+            this.Controls.Add(this.btRandom);
             this.Name = "Form1";
             this.Text = "計算アプリ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Number1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Number2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,14 +116,12 @@ namespace Sample0607 {
 
         #endregion
 
-        private System.Windows.Forms.TextBox tbNum1;
-        private System.Windows.Forms.TextBox tbNum2;
-        private System.Windows.Forms.TextBox tbMod;
+        private System.Windows.Forms.Button btRandom;
+        private System.Windows.Forms.NumericUpDown Number1;
+        private System.Windows.Forms.TextBox text;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox tbAns;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown Number2;
     }
 }
 
--- FormAppSample/Sample0607/Form1.cs
+++ FormAppSample/Sample0607/Form1.cs
@@ -10,23 +10,38 @@ using System.Windows.Forms;
 
 namespace Sample0607 {
     public partial class Form1 : Form {
+        private Random rand = new Random();
+
+        private int randomNumber;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button_Click(object sender, EventArgs e) {
+        private void Form1_Load(object sender, EventArgs e) {
+            //乱数取得 
+            randomNumber = rand.Next(1, (int)Number1.Value);
+            this.Text = randomNumber.ToString();
+        }
 
-            if (int.Parse(tbNum2.Text) != 0)
+        //判定ボタンイベントハンドラ
+        private void btRandom_Click(object sender, EventArgs e) {
+            //入力した値とあらかじめ取得した乱数を比較し結果を表示
+            if (Number2.Value == randomNumber)
             {
-                tbAns.Text= (int.Parse(tbNum1.Text) / int.Parse(tbNum2.Text)).ToString();
-                tbMod.Text= (int.Parse(tbNum1.Text) % int.Parse(tbNum2.Text)).ToString();
+                text.Text = "正解";
             }
-            else {
-                MessageBox.Show("０は除算できません","エラー"
-                    ,MessageBoxButtons.OKCancel
-                    ,MessageBoxIcon.Error );
+            else if (Number2.Value > randomNumber)  {
+                text.Text = "乱数は" + Number2.Value + "より小さいです";
             }
+            else if (Number2.Value < randomNumber) {
+                text.Text = "乱数は" + Number2.Value + "より大きいです";
+            }
+        }
+
+        private void Number1_ValueChanged(object sender, EventArgs e) {
+            randomNumber =rand.Next(1,(int)Number1.Value);
+            this.Text = randomNumber.ToString();
         }
     }
 }
