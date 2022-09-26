f46eddc52bbbfa9b0a1ccf3dd70b5e182bc2f75b
33303新井翔夢 <33303@ojs.ac.jp>
2022-09-26 15:59:30 +0900

    SampleEntityFramework

---
+++ Chapter13/SampleEntitiyFramework/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEntitiyFramework.Models {
+    public class Author {
+        public int ID { get; set; }//主キー
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Gender { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
