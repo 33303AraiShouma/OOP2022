957856da57b0f5ce2feddf650806a6e78dac53ac
33303新井翔夢 <33303@ojs.ac.jp>
2022-10-20 11:59:42 +0900

    SampleUnitConverter

--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -18,20 +18,34 @@ namespace NumberGame {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        int number;
         public MainWindow()
         {
-            int random = new Random().Next(1, 25);
             InitializeComponent();
-             
-            
         }
         
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button bt = (Button)sender;
-            if (bt.Content ==   ) { 
+            if (int.Parse(bt.Content.ToString()) == number)
+            {
+                infoDisp.Text = "正解!";
+                bt.Background = Brushes.Red;
             }
-            
+            else {
+                infoDisp.Text
+                            = int.Parse((string)bt.Content) < number
+                                                          ? "もっと大きいです！" : "もっと小さいです";
+                bt.Background = int.Parse((string)bt.Content) < number
+                                                    ? Brushes.Blue : Brushes.Green;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            infoDisp.Text = "ゲームスタート";
+            number = new Random().Next(1, 25);//正解ナンバーを設定
+            this.Title = number.ToString();//デバック用で正解を表示
         }
     }
 }
---
+++ WPF/SampleUnitConverter/DistanceUnit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter {
+    public class DistanceUnit {
+        public string Name { get; set; }//名称
+        public double Coefficient { get; set; }//係数
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+    //メートル単位を表すクラス
+    public class MetricUnit : DistanceUnit {
+        private static List<MetricUnit> units = new List<MetricUnit> {
+            new MetricUnit{Name = "mm",Coefficient=1,},
+            new MetricUnit{Name = "cm",Coefficient=10,},
+            new MetricUnit{Name = "m",Coefficient=10 * 100,},
+            new MetricUnit{Name = "km",Coefficient=10 * 100 * 1000,},
+        };
+    }
+
+    //ヤード単位を表すクラス
+    public class ImperialUnit : DistanceUnit { 
+    }
+}
