a0bc471b13224c5171aa1c7dabd59d5db9e1e305
33303新井翔夢 <33303@ojs.ac.jp>
2022-10-27 11:56:58 +0900

    ColorCheck途中

---
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CollarChecker {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = GetColorList();
+        }
+        private MyColor[] GetColorList()
+        {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+
+        public class MyColor { 
+            public Color Color { get; set; }
+            public string Name { get; set; }
+        }
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            colorArea.Background = new SolidColorBrush(Color.FromRgb((byte)rSlider.Value,(byte)gSlider.Value,(byte)bSlider.Value));
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+            var color = mycolor.Color;
+            var name = mycolor.Name;
+        }
+
+        private void Border_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
