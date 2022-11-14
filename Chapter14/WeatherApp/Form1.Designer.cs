2ae695833b4431868e370825ac421260d4c8bf75
33303新井翔夢 <33303@ojs.ac.jp>
2022-11-14 15:59:16 +0900

    weather途中

---
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace WeatherApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbWeatherInfo = new System.Windows.Forms.TextBox();
+            this.btWeatherGet = new System.Windows.Forms.Button();
+            this.cbRegion = new System.Windows.Forms.ComboBox();
+            this.地域 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbWeatherInfo
+            // 
+            this.tbWeatherInfo.Location = new System.Drawing.Point(78, 179);
+            this.tbWeatherInfo.Multiline = true;
+            this.tbWeatherInfo.Name = "tbWeatherInfo";
+            this.tbWeatherInfo.Size = new System.Drawing.Size(600, 203);
+            this.tbWeatherInfo.TabIndex = 0;
+            // 
+            // btWeatherGet
+            // 
+            this.btWeatherGet.AccessibleRole = System.Windows.Forms.AccessibleRole.Pane;
+            this.btWeatherGet.Location = new System.Drawing.Point(12, 12);
+            this.btWeatherGet.Name = "btWeatherGet";
+            this.btWeatherGet.Size = new System.Drawing.Size(124, 50);
+            this.btWeatherGet.TabIndex = 1;
+            this.btWeatherGet.Text = "取得";
+            this.btWeatherGet.UseVisualStyleBackColor = true;
+            this.btWeatherGet.Click += new System.EventHandler(this.btWeatherGet_Click);
+            // 
+            // cbRegion
+            // 
+            this.cbRegion.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbRegion.FormattingEnabled = true;
+            this.cbRegion.Location = new System.Drawing.Point(385, 21);
+            this.cbRegion.Name = "cbRegion";
+            this.cbRegion.Size = new System.Drawing.Size(228, 41);
+            this.cbRegion.TabIndex = 2;
+            // 
+            // 地域
+            // 
+            this.地域.AutoSize = true;
+            this.地域.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.地域.Location = new System.Drawing.Point(266, 27);
+            this.地域.Name = "地域";
+            this.地域.Size = new System.Drawing.Size(85, 35);
+            this.地域.TabIndex = 3;
+            this.地域.Text = "地域";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "天気概要";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 382);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.地域);
+            this.Controls.Add(this.cbRegion);
+            this.Controls.Add(this.btWeatherGet);
+            this.Controls.Add(this.tbWeatherInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbWeatherInfo;
+        private System.Windows.Forms.Button btWeatherGet;
+        private System.Windows.Forms.ComboBox cbRegion;
+        private System.Windows.Forms.Label 地域;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Chapter14/WeatherApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace WeatherApp {
+    public partial class Form1 : Form {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btWeatherGet_Click(object sender, EventArgs e) {
+            var wc = new WebClient()
+            {
+                Encoding = Encoding.UTF8
+            };
+            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/common/const/area.json");
+
+            var json = JsonConvert.DeserializeObject<Rootobject>(dString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(@"areacode.csv",System.Text.Encoding.GetEncoding("shift_jis"));
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] values = line.Split(',');
+                    List<string> lists = new List<string>();
+                    lists.AddRange(values);
+                    
+                    foreach (var item in lists)
+                    {
+                        
+                        cbRegion.Items.Add(item);
+                    }
+
+                }
+            }
+        }
+    }
+ }
